e6e46443419d98ff16d02388954a1530c3d35fa4
Pakkapong2 <pmiswnnil@gmail.com>
2025-02-27 12:05:20 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const carRoute = require("./routes/car.route");
-const registerRoute = require("./routes/register.route")
-
+const authRoute = require("./routes/auth.route")
+const bookingRoute = require("./routes/booking.route")
+const fineRoute = require("./routes/fine.route")
 
 
 app.use('/images', express.static('images'));
@@ -19,7 +20,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/cars", carRoute);
-app.use("/registers", registerRoute);
+app.use("/auth", authRoute);
+app.use("/booking", bookingRoute);
+app.use("/fine", fineRoute);
 
 app.listen(port, () => {
   console.log("App started at port: " + port);
